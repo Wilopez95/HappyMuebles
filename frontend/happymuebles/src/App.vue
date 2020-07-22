43c1190c8043905f1915c9740a533ecbfed8675e
Wilson <Wilopez95@gmail.com>
2020-07-22 14:12:21 -0600

    new frontend part

---
+++ frontend/happymuebles/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin: 0px;
+  color: #2c3e50;
+  background: #A97E51;
+}
+</style>
---
+++ frontend/happymuebles/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="navbar">
+        <ul>
+            <li><a href=#>Armarios</a></li>
+            <li><a href=#>Alavenas</a></li>
+            <li><a href=#>Camas</a></li>
+            <li><a href=#>Escritorios</a></li>
+            <li><a href=#>Librerias</a></li>
+            <li><a href=#>Comodas</a></li>
+            <li><a href=#>Otros</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
