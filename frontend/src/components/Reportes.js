8b5a9309b64f3bd2fac9e0ce3c621c503411d3ba
maria <maria2000jose.mj@gmail.com>
2020-08-15 17:13:00 -0600

    historial por sucursal para los admin

--- frontend/src/components/Reportes.js
+++ frontend/src/components/Reportes.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import CalificarCompra from './CalificarCompra'
 
 export default class RegisterEmployee extends Component {
-/*
+
     state = {
         compras: [],
+        sucursal :1,
         seen: false,
         pkCompra: 0
     }
@@ -16,28 +19,63 @@ export default class RegisterEmployee extends Component {
     }
 
     componentDidMount() {
-       //this.getClientInfo()
-       this.getHistory()
-        
+      // this.getHistory()
     }
 
+   async getHistory(idSucursal){
 
-    /*async getClientInfo(){
-        const res = await axios.get('http://localhost:3300/api/costumer/info/get/'+localStorage.getItem('idCliente') );
-        console.log(res)
-    }*/
-
- /*  async getHistory(){
-
-        const res = await axios.get('http://localhost:3300/api/purchase/client/history/'+localStorage.getItem('idCliente') );
+        const res = await axios.get('http://localhost:3300/api/purchase/sucursal/history/'+idSucursal);
         this.setState({ compras: res.data });
         console.log(res.data)
-    }*/
+    }
+    onChangeSucursal = e => {
+        console.log(e.target.value);
+        this.setState({
+            sucursal:e.target.value
+        })
+
+        this.getHistory(e.target.value);
+    }
 
 
     render() {
         return (
-            <h4>Ventana para consultar reportes(disponible solo para el admin)</h4>
+            <div className="row">
+            <div className="col-md-4">
+                <div className="card card-body">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <h4 className="text-center">Filtros por Categoria</h4>
+                        </div>
+                        <div className="form-group">
+                            <select value={this.state.value} onChange={this.onChangeSucursal}>
+                                <option>Escoga una sucursal</option>
+                                <option value="1">Sucursal 1</option>
+                                <option value="2">Sucursal 2</option>
+                                <option value="3">Sucursal 3</option>
+                            </select>
+                        </div>
+                        <div className="row">
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="col-md-8">
+            <h1 className="text-center">Historial de compras</h1>
+                    <ul className="list-group">
+                        {
+                            this.state.compras.map(compra => <li className="list-group-item list-group-item-action" key={compra.pkCompra}>
+                                    <p>IdCompra: {compra.pkCompra}</p>
+                                    <p>Fecha compra: {compra.FechaCompra}</p>
+                                    <p>Precio de la compra: {compra.MontoTotal}₡</p>
+                                    <p>Sucursal de compra: {compra.NombreSucursal}</p>
+                                    <p>Metodo de pago:{compra.MetodoPago}</p>                              
+                            </li>)
+                        }
+                        
+                    </ul>
+            </div>
+        </div>
         )
     }
 }
