bbbda0c88050e698650df02355d7946032b2271c
jjomena <joaquinmena.84@gmail.com>
2020-08-15 16:26:06 -0600

    Actuliazr empleado profile

--- frontend/src/components/empleadoProfile.js
+++ frontend/src/components/empleadoProfile.js
@@ -10,7 +10,7 @@ export default class empleadoProfile extends Component {
         return (
             <div className="row">
                 <div className="col-md-4">
-                    <updateEmpleado/>
+                    <UpdateEmpleado/>
                 </div>
                 <div className="col-md-8">
                 <h1 className="text-center">Historial de Ventas</h1>
