77ae021cca2fa8875466b90a489cc3d9bc0c24d1
maria <maria2000jose.mj@gmail.com>
2020-08-15 15:54:06 -0600

    fix updateEmpleado-historialXSucursal

--- frontend/src/components/Reportes.js
+++ frontend/src/components/Reportes.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react'
 
 export default class RegisterEmployee extends Component {
+/*
+    state = {
+        compras: [],
+        seen: false,
+        pkCompra: 0
+    }
+
+    viewMore = (pkcompra) => {
+        this.setState({
+            seen: !this.state.seen,
+            pkCompra: pkcompra
+        });
+    }
+
+    componentDidMount() {
+       //this.getClientInfo()
+       this.getHistory()
+        
+    }
+
+
+    /*async getClientInfo(){
+        const res = await axios.get('http://localhost:3300/api/costumer/info/get/'+localStorage.getItem('idCliente') );
+        console.log(res)
+    }*/
+
+ /*  async getHistory(){
+
+        const res = await axios.get('http://localhost:3300/api/purchase/client/history/'+localStorage.getItem('idCliente') );
+        this.setState({ compras: res.data });
+        console.log(res.data)
+    }*/
+
 
     render() {
         return (
